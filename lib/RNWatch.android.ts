de956d196bea4389832e13a848209b76a409a513
Marten Homma <marten@canvasheroes.com>
2021-04-22 10:16:13 +0200

    Removed unusable android file

--- lib/RNWatch.android.ts
+++ lib/RNWatch.android.ts
@@ -1,16 +1,15 @@
-/**
- * Stub of RNWatch for Android.
- *
- * @providesModule RNWatch
- */
-'use strict';
+export * from './messages';
+export * from './message-data';
+export * from './reachability';
+export {startFileTransfer, getFileTransfers} from './files';
+export type {FileTransfer} from './files';
+export * from './user-info';
+export * from './application-context';
+export * from './hooks';
+export type {WatchPayload} from './native-module';
+export * from './errors';
 
-const warning = require('fbjs/lib/warning');
-
-const RNWatch = {
-  test: function () {
-    warning('Not supported on Android.');
-  },
-};
-
-export default RNWatch;
+export {default as watchEvents} from './events';
+export type {WatchEvent} from './events/definitions';
+export {getIsPaired} from './paired';
+export {getIsWatchAppInstalled} from './installed';
